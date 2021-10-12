1c0837d1116035b729e96f6004e13ef5637ffd10
the-j-curl <jamie.p.cook@outlook.com>
2021-10-12 11:48:28 +0200

    Completed first 2 challanges & tested. Fixed typo

--- add/add.ts
+++ add/add.ts
@@ -1,12 +1,15 @@
 export function add(param1: number, param2: number): number {
-
-}
+  return param1 + param2;
+};
 
 export function add2(...param1: number[]): number {
+  return param1.reduce((totalValue, currentValue) => {
+    return totalValue + currentValue;
+  },0);
+};
 
-}
-// console.log(add(1, 2));
-// console.log(add(3, 2));
+console.log(add(1, 2));
+console.log(add(3, 2));
 
-// console.log(add2(1,2,3,4,5));
-// console.log(add2(2,3));
+console.log(add2(1,2,3,4,5));
+console.log(add2(2,3));
