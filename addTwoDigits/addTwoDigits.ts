80ac3a57cb08f7f8c59479e31a40b5d7e9d83d10
the-j-curl <jamie.p.cook@outlook.com>
2021-10-12 13:27:23 +0200

    Completed challenge 4 with alt solution

--- addTwoDigits/addTwoDigits.ts
+++ addTwoDigits/addTwoDigits.ts
@@ -1,5 +1,15 @@
 export function addTwoDigits(n: any): number {
+return n.toString().split('').reduce((totalValue: string, currentValue: string) => {
+  return +totalValue + +currentValue;
+});
+};
 
-}
+console.log(addTwoDigits(29));
 
-// console.log(addTwoDigits(29));
+// alternative solution
+// export function addTwoDigits(n: any): number {
+//   const numberArray = n.toString().split('');
+//   return +numberArray[0] + +numberArray[1];
+//   };
+  
+//   console.log(addTwoDigits(29));
