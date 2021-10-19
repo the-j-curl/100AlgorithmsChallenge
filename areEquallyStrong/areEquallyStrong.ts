7843579b61ed3e73b6560f67d2acfd1933717d8c
the-j-curl <jamie.p.cook@outlook.com>
2021-10-19 16:41:20 +0200

    equally strong completed

--- areEquallyStrong/areEquallyStrong.ts
+++ areEquallyStrong/areEquallyStrong.ts
@@ -1,7 +1,22 @@
-export function areEquallyStrong(yourLeft: number, yourRight: number, friendsLeft: number, friendsRight: number): boolean {
+export function areEquallyStrong(
+	yourLeft: number,
+	yourRight: number,
+	friendsLeft: number,
+	friendsRight: number
+): boolean {
+	const yourStrongestArm = yourLeft >= yourRight ? yourLeft : yourRight
+	const friendsStrongestArm =
+		friendsLeft >= friendsRight ? friendsLeft : friendsRight
+	const yourWeakestArm = yourLeft <= yourRight ? yourLeft : yourRight
+	const friendsWeakestArm =
+		friendsLeft <= friendsRight ? friendsLeft : friendsRight
 
+	return yourStrongestArm === friendsStrongestArm &&
+		yourWeakestArm === friendsWeakestArm
+		? true
+		: false
 }
 
-// console.log(areEquallyStrong(10, 15, 15, 10))
-// console.log(areEquallyStrong(15, 10, 15, 10))
-// console.log(areEquallyStrong(15, 10, 15, 9))
+console.log(areEquallyStrong(10, 15, 15, 10))
+console.log(areEquallyStrong(15, 10, 15, 10))
+console.log(areEquallyStrong(15, 10, 15, 9))
