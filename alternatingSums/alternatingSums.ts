49a6bf91ce388cf7d95de82703ead80b85a1ba8c
the-j-curl <jamie.p.cook@outlook.com>
2021-10-19 16:04:39 +0200

    alternating sums challange complete & tested

--- alternatingSums/alternatingSums.ts
+++ alternatingSums/alternatingSums.ts
@@ -1,5 +1,15 @@
 export function alternatingSums(a: number[]): number[] {
+	let evenTotal = 0
+	let oddTotal = 0
+	a.forEach((number, index) => {
+		if (index % 2 === 0) {
+			evenTotal = evenTotal += number
+		} else {
+			oddTotal += number
+		}
+	})
 
+	return [evenTotal, oddTotal]
 }
 
 console.log(alternatingSums([50, 60, 60, 45, 70]))
