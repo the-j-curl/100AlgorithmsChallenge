8b387855485643ad342f3635ee55875858440008
the-j-curl <jamie.p.cook@outlook.com>
2021-10-22 11:02:25 +0200

    allLongestStrings completed and passed testing

--- allLongestStrings/allLongestStrings.ts
+++ allLongestStrings/allLongestStrings.ts
@@ -1,5 +1,19 @@
 export function allLongestStrings(inputArray: string[]): string[] {
+	let longestStringLength = 0
+	const longestWords = []
 
+	for (let i = 0; i < inputArray.length; i++) {
+		longestStringLength =
+			inputArray[i].length > longestStringLength
+				? inputArray[i].length
+				: longestStringLength
+
+		if (inputArray[i].length === longestStringLength) {
+			longestWords.push(inputArray[i])
+		}
+	}
+
+	return longestWords
 }
 
-console.log(allLongestStrings(["aba", "aa", "ad", "vcd", "aba"]));
+console.log(allLongestStrings(['aba', 'aa', 'ad', 'vcd', 'aba']))
