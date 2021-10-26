e5f94cf3cf8d344826a133df91a6f30d5d5be5c5
the-j-curl <jamie.p.cook@outlook.com>
2021-10-26 11:14:53 +0200

    characterParity completed and passed testing

--- characterParity/characterParity.ts
+++ characterParity/characterParity.ts
@@ -1,7 +1,8 @@
 export function characterParity(symbol: string): string {
-
+	const number = parseInt(symbol)
+	return isNaN(number) ? 'not a digit' : number % 2 === 0 ? 'even' : 'odd'
 }
 
-// console.log(characterParity('5'))
-// console.log(characterParity('8'))
-// console.log(characterParity('q'))
+console.log(characterParity('5'))
+console.log(characterParity('8'))
+console.log(characterParity('q'))
