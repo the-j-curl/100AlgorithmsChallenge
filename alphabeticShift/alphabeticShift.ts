fbc352f9c73adea207ced8ab5aca2000cab9479a
the-j-curl <jamie.p.cook@outlook.com>
2021-11-01 11:53:44 +0100

    Alphabet shift completed and passed testing

--- alphabeticShift/alphabeticShift.ts
+++ alphabeticShift/alphabeticShift.ts
@@ -1,5 +1,42 @@
 export function alphabeticShift(inputString: string): string {
+	const alphabet: string[] = [
+		'a',
+		'b',
+		'c',
+		'd',
+		'e',
+		'f',
+		'g',
+		'h',
+		'i',
+		'j',
+		'k',
+		'l',
+		'm',
+		'n',
+		'o',
+		'p',
+		'q',
+		'r',
+		's',
+		't',
+		'u',
+		'v',
+		'w',
+		'x',
+		'y',
+		'z',
+	]
 
+	let inputShift: string[] = inputString.split('')
+	for (let i = 0; i < inputShift.length; i++) {
+		let indexValue = 0
+		if (inputShift[i] !== 'z') {
+			indexValue = alphabet.indexOf(inputShift[i]) + 1
+		}
+		inputShift[i] = alphabet[indexValue]
+	}
+	return inputShift.join('')
 }
 
-console.log(alphabeticShift('crazy'));
+console.log(alphabeticShift('crazy'))
