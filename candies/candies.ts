787fc6032549f5b2b5439f3eeb07cf24d1b71040
the-j-curl <jamie.p.cook@outlook.com>
2021-11-01 12:08:58 +0100

    Candies challange completed

--- candies/candies.ts
+++ candies/candies.ts
@@ -1,5 +1,7 @@
 export function candies(n: number, m: number): number {
-
+	const candiesRemaining = m % n
+	return m - candiesRemaining
 }
 
-// console.log(candies(3, 10));
+console.log(candies(3, 10))
+console.log(candies(5, 20))
