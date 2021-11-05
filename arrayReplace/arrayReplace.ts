4f07fcceaaf4da612b0a0e9d8baee4de4a7de845
the-j-curl <jamie.p.cook@outlook.com>
2021-11-05 10:20:58 +0100

    Simplified solution

--- arrayReplace/arrayReplace.ts
+++ arrayReplace/arrayReplace.ts
@@ -1,5 +1,29 @@
-export function arrayReplace(inputArray: number[], elemToReplace: number, substitutionElem: number): number[] {
-
+export function arrayReplace(
+	inputArray: number[],
+	elemToReplace: number,
+	substitutionElem: number
+): number[] {
+	inputArray.forEach((number, index) => {
+		if (number === elemToReplace) {
+			inputArray[index] = substitutionElem
+		}
+	})
+	return inputArray
 }
 
-// console.log(arrayReplace([1, 2, 1], 1, 3));
+console.log(arrayReplace([1, 2, 1], 1, 3))
+
+// Alternative solution
+
+// export function arrayReplace(
+// 	inputArray: number[],
+// 	elemToReplace: number,
+// 	substitutionElem: number
+// ): number[] {
+// 	inputArray.forEach((num: number, index: number) => {
+// 		if (num === elemToReplace) {
+// 			return inputArray.splice(index, 1, substitutionElem)
+// 		}
+// 	})
+// 	return inputArray
+// }
