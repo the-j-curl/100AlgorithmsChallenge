897b827d3e0b70bdbbc7cf84b46466563dce6c03
the-j-curl <jamie.p.cook@outlook.com>
2021-11-05 15:40:48 +0100

    Completed deposit challange and tested

--- depositProfit/depositProfit.ts
+++ depositProfit/depositProfit.ts
@@ -1,5 +1,17 @@
-export function depositProfit(deposit: number, rate: number, threshold: number): number {
+export function depositProfit(
+	deposit: number,
+	rate: number,
+	threshold: number
+): number {
+	let years: number = 0
+	let balance = deposit
 
+	while (threshold > balance) {
+		balance += balance * (rate / 100)
+		years++
+	}
+
+	return years
 }
 
-// console.log(depositProfit(100, 20, 170))
+console.log(depositProfit(100, 20, 170))
