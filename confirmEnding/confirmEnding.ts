d511436743063276f017a104c531685509e576c6
the-j-curl <jamie.p.cook@outlook.com>
2021-11-05 13:27:27 +0100

    Completed challange, corrected test file name

--- confirmEnding/confirmEnding.ts
+++ confirmEnding/confirmEnding.ts
@@ -1,6 +1,8 @@
 export function confirmEnding(str: string, target: string) {
-
+	const startPoint = str.length - target.length
+	const stringEnd = str.substr(startPoint)
+	return stringEnd === target
 }
 
-// console.log(confirmEnding("Abstraction", "action"));
-// console.log(confirmEnding("Open sesame", "pen"));
+console.log(confirmEnding('Abstraction', 'action'))
+console.log(confirmEnding('Open sesame', 'pen'))
