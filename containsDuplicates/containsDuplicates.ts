6afe332ca8b02864ba2d287d2d1cce58e83902c7
the-j-curl <jamie.p.cook@outlook.com>
2021-11-05 09:24:47 +0100

    Refactored code

--- containsDuplicates/containsDuplicates.ts
+++ containsDuplicates/containsDuplicates.ts
@@ -1,7 +1,19 @@
 export function containsDuplicates(a: number[]): boolean {
-	const singleArray = [...new Set(a)]
-	return singleArray.length !== a.length
+	a.sort((a, b) => a - b)
+	for (let i = 0; i < a.length; i++) {
+		if (a[i] === a[i + 1]) {
+			return true
+		}
+		return false
+	}
 }
 
 console.log(containsDuplicates([1, 2, 3, 1]))
 console.log(containsDuplicates([3, 1]))
+
+// Alternative solution
+
+// export function containsDuplicates(a: number[]): boolean {
+// 	const singleArray = [...new Set(a)]
+// 	return singleArray.length !== a.length
+// }
