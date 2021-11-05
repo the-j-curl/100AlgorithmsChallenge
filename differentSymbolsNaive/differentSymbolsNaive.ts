8efd613ae5e5e451eee2779fa25011761fd85702
the-j-curl <jamie.p.cook@outlook.com>
2021-11-05 15:26:09 +0100

    refactored

--- differentSymbolsNaive/differentSymbolsNaive.ts
+++ differentSymbolsNaive/differentSymbolsNaive.ts
@@ -1,6 +1,5 @@
 export function differentSymbolsNaive(s: string): number {
-	const uniqueChars: string[] = [...new Set(s)]
-	return uniqueChars.length
+	return new Set(s).size
 }
 
 console.log(differentSymbolsNaive('cabca'))
