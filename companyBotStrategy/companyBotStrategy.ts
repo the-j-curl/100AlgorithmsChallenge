1c45ce2bf600ac30291860c668011c82876bf754
the-j-curl <jamie.p.cook@outlook.com>
2021-11-05 11:28:04 +0100

    Completed challange and passed testing

--- companyBotStrategy/companyBotStrategy.ts
+++ companyBotStrategy/companyBotStrategy.ts
@@ -1,7 +1,41 @@
 export function companyBotStrategy(trainingData: number[][]): number {
+	let time = 0
+	let scoreArray = []
 
+	trainingData.forEach((data: number[]) => {
+		if (data[1] === 1) {
+			scoreArray.push(data[0])
+		}
+		if (scoreArray.length > 0) {
+			time = scoreArray.reduce((total, currenValue) => {
+				return (total += currenValue)
+			})
+		}
+	})
+
+	return time / scoreArray.length || 0
 }
 
-// console.log(companyBotStrategy([[3, 1], [6, 1], [4, 1], [5, 1]]));
-// console.log(companyBotStrategy([[4, 1], [4, -1], [0, 0], [6, 1]]));
-// console.log(companyBotStrategy( [[4, -1], [0, 0], [5, -1]]));
+console.log(
+	companyBotStrategy([
+		[3, 1],
+		[6, 1],
+		[4, 1],
+		[5, 1],
+	])
+)
+console.log(
+	companyBotStrategy([
+		[4, 1],
+		[4, -1],
+		[0, 0],
+		[6, 1],
+	])
+)
+console.log(
+	companyBotStrategy([
+		[4, -1],
+		[0, 0],
+		[5, -1],
+	])
+)
