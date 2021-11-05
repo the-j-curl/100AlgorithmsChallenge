c9109d82842317e2aeb84036e0d462224d1d68c5
the-j-curl <jamie.p.cook@outlook.com>
2021-11-05 16:10:06 +0100

    domainType completed and tested

--- domainType/domainType.ts
+++ domainType/domainType.ts
@@ -1,5 +1,26 @@
 export function domainType(domains: string[]): string[] {
-
+	const domainTypes: string[] = []
+	domains.forEach(string => {
+		const startPoint = string.lastIndexOf('.')
+		const currentDomain = string.substr(startPoint)
+		switch (currentDomain) {
+			case '.org':
+				domainTypes.push('organization')
+				break
+			case '.com':
+				domainTypes.push('commercial')
+				break
+			case '.net':
+				domainTypes.push('network')
+				break
+			case '.info':
+				domainTypes.push('information')
+				break
+		}
+	})
+	return domainTypes
 }
 
-// console.log(domainType(["en.wiki.org", "codefights.com", "happy.net", "code.info"]));
+console.log(
+	domainType(['en.wiki.org', 'codefights.com', 'happy.net', 'code.info'])
+)
