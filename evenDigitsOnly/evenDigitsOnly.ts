303e36542601d3dda94d2fd1e2379eef5a76f877
the-j-curl <jamie.p.cook@outlook.com>
2021-11-08 14:17:32 +0100

    Refactored solution

--- evenDigitsOnly/evenDigitsOnly.ts
+++ evenDigitsOnly/evenDigitsOnly.ts
@@ -1,14 +1,6 @@
 export function evenDigitsOnly(n: number): boolean {
-	const numArray = n.toString().split('')
-	let evenArray: number[] = []
-
-	numArray.forEach(num => {
-		if (+num % 2 === 0) {
-			evenArray.push(+num)
-		}
-	})
-	return numArray.length === evenArray.length
+	const digitArray = n.toString().split('')
+	return digitArray.every(digit => +digit % 2 === 0)
 }
-
 console.log(evenDigitsOnly(248622))
 console.log(evenDigitsOnly(642386))
