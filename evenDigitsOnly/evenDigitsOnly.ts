a27c1149d64a6cd2740815f53e97a6d259838d0f
the-j-curl <jamie.p.cook@outlook.com>
2021-11-08 14:13:00 +0100

    Completed and passed testing

--- evenDigitsOnly/evenDigitsOnly.ts
+++ evenDigitsOnly/evenDigitsOnly.ts
@@ -1,6 +1,14 @@
 export function evenDigitsOnly(n: number): boolean {
+	const numArray = n.toString().split('')
+	let evenArray: number[] = []
 
+	numArray.forEach(num => {
+		if (+num % 2 === 0) {
+			evenArray.push(+num)
+		}
+	})
+	return numArray.length === evenArray.length
 }
 
-// console.log(evenDigitsOnly(248622));
-// console.log(evenDigitsOnly(642386));
+console.log(evenDigitsOnly(248622))
+console.log(evenDigitsOnly(642386))
