d3af82f3a49551b0525cbac2f8f1f8a7ee3c8a44
the-j-curl <jamie.p.cook@outlook.com>
2021-11-08 14:25:50 +0100

    Challange completed and passed testing

--- extractEachKth/extractEachKth.ts
+++ extractEachKth/extractEachKth.ts
@@ -1,4 +1,8 @@
 export function extractEachKth(inputArray: number[], k: number): number[] {
+	const filteredArray = inputArray.filter(
+		(number, index) => (index + 1) % k !== 0
+	)
+	return filteredArray
 }
 
-// console.log(extractEachKth([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3));
+console.log(extractEachKth([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3))
