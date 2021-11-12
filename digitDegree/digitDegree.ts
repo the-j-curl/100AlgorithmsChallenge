b25d30f0a26922b259c4c4dbc70b98a5b83209b4
the-j-curl <jamie.p.cook@outlook.com>
2021-11-12 09:20:42 +0100

    Completed challenge and tested

--- digitDegree/digitDegree.ts
+++ digitDegree/digitDegree.ts
@@ -1,7 +1,23 @@
-export function digitDegree(n: number): number {
+export const digitDegree = (n: number): number => {
+	let count = 0
 
+	if (n <= 9) {
+		return count
+	}
+
+	const nArray = n.toString().split('')
+
+	for (let i = 0; i < nArray.length; i++) {
+		const sum = parseInt(nArray[i]) + parseInt(nArray[i + 1])
+		count++
+		if (sum < 10) {
+			return count
+		}
+	}
+
+	return count
 }
 
-// console.log(digitDegree(5));
-// console.log(digitDegree(10));
-// console.log(digitDegree(91));
+console.log(digitDegree(5))
+console.log(digitDegree(100))
+console.log(digitDegree(91))
