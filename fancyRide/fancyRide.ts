d944aa63d1fa37cd09fc67128bf2dba942c86819
the-j-curl <jamie.p.cook@outlook.com>
2021-11-15 13:30:06 +0100

    Algorithms completed and passed testing

--- fancyRide/fancyRide.ts
+++ fancyRide/fancyRide.ts
@@ -1,5 +1,36 @@
-export function fancyRide(l: number, fares: number[]): string {
+export const fancyRide = (l: number, fares: number[]): string => {
+	let i: number = 0
+	const uberRide: string[] = [
+		'UberX',
+		'UberXL',
+		'UberPlus',
+		'UberBlack',
+		'UberSUV',
+	]
 
+	fares
+		.map(fare => {
+			return fare * l
+		})
+		.forEach((ride: number, index: number) => {
+			if (ride <= 20 && index === 0) {
+				return (i = 0)
+			}
+			if (ride <= 20 && index === 1) {
+				return (i = 1)
+			}
+			if (ride <= 20 && index === 2) {
+				return (i = 2)
+			}
+			if (ride <= 20 && index === 3) {
+				return (i = 3)
+			}
+			if (ride <= 20 && index === 4) {
+				return (i = 4)
+			} else return null
+		})
+
+	return uberRide[i]
 }
 
-// console.log(fancyRide(30, [0.3, 0.5, 0.7, 1, 1.3]));
+console.log(fancyRide(30, [0.3, 0.5, 0.7, 1, 1.3]))
