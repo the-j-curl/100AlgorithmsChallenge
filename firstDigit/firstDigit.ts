743198c8a441cbc18dad0ad0d65e2c81320a78a8
the-j-curl <jamie.p.cook@outlook.com>
2021-11-22 16:13:38 +0100

    Completed and tested

--- firstDigit/firstDigit.ts
+++ firstDigit/firstDigit.ts
@@ -1,7 +1,15 @@
-export function firstDigit(inputString: string): string {
-
+export const firstDigit = (inputString: string): string => {
+	const inputArray: string[] = inputString.split('')
+	let digit = ''
+	inputArray.forEach(char => {
+		if (digit === '' && !isNaN(parseInt(char))) {
+			digit = char
+		}
+	})
+	return digit
 }
 
-// console.log(firstDigit('var_1__Int'));
-// console.log(firstDigit('q2q-q'));
-// console.log(firstDigit('0ss'));
+console.log(firstDigit('var_1__Int'))
+console.log(firstDigit('q2q-q'))
+console.log(firstDigit('0ss'))
+console.log(firstDigit('Jam1e C00k'))
