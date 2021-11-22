826b0b46bc3f58065a1046ab5313f0f3c240cea2
the-j-curl <jamie.p.cook@outlook.com>
2021-11-22 15:54:10 +0100

    Completed and passed testing

--- commonCharacterCount/commonCharacterCount.ts
+++ commonCharacterCount/commonCharacterCount.ts
@@ -1,5 +1,35 @@
-export function commonCharacterCount(s1: string, s2: string): number {
+export const commonCharacterCount = (s1: string, s2: string): number => {
+	const string1Arr: string[] = s1.split('')
+	const string2Arr: string[] = s2.split('')
+	const s1CharCount = getCharList(string1Arr)
+	const s2CharCount = getCharList(string2Arr)
+	let total = 0
 
+	for (const prop in s1CharCount) {
+		if (s2CharCount.hasOwnProperty(prop)) {
+			if (s2CharCount[prop] < s1CharCount[prop]) {
+				total += s2CharCount[prop]
+			} else {
+				total += s1CharCount[prop]
+			}
+		}
+	}
+
+	return total
 }
 
-// console.log(commonCharacterCount('aabcc', 'adcaa'));
+const getCharList = (chars: string[]): object => {
+	const wordCount = {}
+
+	chars.forEach(char => {
+		if (wordCount.hasOwnProperty(char)) {
+			wordCount[char]++
+		} else {
+			wordCount[char] = 1
+		}
+	})
+
+	return wordCount
+}
+
+console.log(commonCharacterCount('aabcc', 'adcaa'))
