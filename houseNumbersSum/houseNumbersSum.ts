1f24ebc853679a74f7dcaf7173a1aaff921b83bd
the-j-curl <jamie.p.cook@outlook.com>
2021-11-25 14:01:55 +0100

    Challenge completed

--- houseNumbersSum/houseNumbersSum.ts
+++ houseNumbersSum/houseNumbersSum.ts
@@ -1,5 +1,12 @@
-export function houseNumbersSum(inputArray: number[]): number {
-
+export const houseNumbersSum = (inputArray: number[]): number => {
+	let total = 0
+	for (let i = 0; i < inputArray.length; i++) {
+		total += inputArray[i]
+		if (inputArray[i] === 0) {
+			return total
+		}
+	}
+	return total
 }
 
-// console.log(houseNumbersSum([5, 1, 2, 3, 0, 1, 5, 0, 2]));
+console.log(houseNumbersSum([5, 1, 2, 3, 0, 1, 5, 0, 2]))
