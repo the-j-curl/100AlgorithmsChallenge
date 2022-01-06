cac03bec991259041689ae71151fa0e76a08ae18
the-j-curl <jamie.p.cook@outlook.com>
2022-01-06 11:23:22 +0100

    Missing letters algorithm finally completed!

--- missingLetters/missingLetters.ts
+++ missingLetters/missingLetters.ts
@@ -1,8 +1,21 @@
-export function missingLetters(str: string): string {
+export const missingLetters = (str: string): string => {
+	const charValues: number[] = str.split('').map((char: string) => {
+		return char.charCodeAt(0)
+	})
 
+	if (charValues[0] !== 97) {
+		return 'a'
+	}
+
+	for (let i = 0; i < charValues.length - 1; i++) {
+		if (charValues[i] !== charValues[i + 1] - 1) {
+			return String.fromCharCode(charValues[i] + 1)
+		}
+	}
+	return undefined
 }
 
-// console.log(missingLetters("bce"));
-// console.log(missingLetters("abce"));
-// console.log(missingLetters("abcdefghjklmno"));
-// console.log(missingLetters("abcdefghijklmnopqrstuvwxyz"));
+console.log(missingLetters('bce'))
+console.log(missingLetters('abce'))
+console.log(missingLetters('abcdefghjklmno'))
+console.log(missingLetters('abcdefghijklmnopqrstuvwxyz'))
